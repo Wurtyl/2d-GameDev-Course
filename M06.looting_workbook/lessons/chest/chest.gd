2b48747f0c860a580d29975da0cec1a96260e8bb
Wurtyl <hadavis1@s.sfusd.edu>
2024-05-07 10:12:09 -0700

    M.6 / L.5 - Open The Chest + Practices 1-2 / Complete

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @onready var canvas_group = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var is_open := false
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -19,3 +21,21 @@ func _on_mouse_exited():
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	if animation_player.assigned_animation == "open":
+		return
+	if is_open:
+		return
+	is_open = true
+	animation_player.play("open")
+	input_pickable = false
